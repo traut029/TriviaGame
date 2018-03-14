10b2c8f1dac8e98db75f4696671a319a4675dd67
Eric Trautmiller <trautmillereahs@gmail.com>
2018-03-13 22:06:37 -0500

    further along, maybe rewrite js to be shorter and less messy

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,9 +8,25 @@ var question1 = {
     a3: "summinsuch",
     a4: "pie"
 };
-var pauseTimer = false;
+
+var question2 = {
+    q: "questioning question2?",
+    a1: "idk2",
+    a2: "can you repeat the question2?",
+    a3: "summinsuch2",
+    a4: "pie2"
+};
+var question3 = {
+    q: "questioning question3?",
+    a1: "idk3",
+    a2: "can you repeat the question3?",
+    a3: "summinsuch3",
+    a4: "pie3"
+};
 
 
+var answersRight = 0;
+var answersGuessed = 0;
 
 //wait until document loads before doing anything
 $(document).ready(function () {
@@ -23,13 +39,11 @@ $(document).ready(function () {
 
 
         if (start == 1) {
-            setInterval(timer, 1000);
+            intervalId = setInterval(timer, 1000);
 
             slide1();
 
         }
-
-
     })
 
 
@@ -42,7 +56,7 @@ $(document).ready(function () {
 //initial set up of first page
 function startPage() {
     console.log("hello")
-    $("#buttonDiv").append('<button id="startButton">Start<button>');
+    $("#buttonDiv").append('<button id="startButton">Start</button>');
     $("#startButton").click(function () {
         start = 1;
 
@@ -59,24 +73,54 @@ function timer() {
     console.log(count);
 
     //stop before hitting zero
-    if (count <= 0) {
-
-
-        //text frozen but counter still going
-        return;
+    if (count <= 0 && answersGuessed == 0) {
+        stop();
+        $("#question").text("Out of time!");
+        $("#answer1").text("The correct answer was your face.");
+        $("#answer2").text("");
+        $("#answer3").text("");
+        $("#answer4").text("");
+        pause1();
+    }
+    if (count <= 0 && answersGuessed == 1) {
+        stop();
+        $("#question").text("Out of time2!");
+        $("#answer1").text("The correct answer was your face2.");
+        $("#answer2").text("");
+        $("#answer3").text("");
+        $("#answer4").text("");
+        pause2();
+    }
+    if (count <= 0 && answersGuessed == 2) {
+        stop();
+        $("#question").text("Out of time3!");
+        $("#answer1").text("The correct answer was your face3.");
+        $("#answer2").text("");
+        $("#answer3").text("");
+        $("#answer4").text("");
 
     }
-    //pauses VISUAL of timer on screen with click of answer
+
+
+
+    //pauses timer on screen with click of answer
     $(".answer").click(function () {
-        pauseTimer = true;
+        stop();
+
+
     })
 
     //shows timer on screen
-    if (pauseTimer == false) {
-        $("#timer").text("Time Remaining: " + count + " Seconds");
-    }
-}
 
+    $("#timer").text("Time Remaining: " + count + " Seconds");
+
+}
+//stop function
+function stop() {
+    clearInterval(intervalId);
+    start = 2;
+
+}
 
 
 
@@ -91,12 +135,138 @@ function slide1() {
     $("#answer4").text(question1.a4);
     //will remove q and a text if an answer is clicked and freezes time
     $(".answer").click(function () {
-        $("#question").text("");
-        $("#answer1").text("");
-        $("#answer2").text("");
-        $("#answer3").text("");
-        $("#answer4").text("");
+        if (answersGuessed == 0) {
+            $("#question").text("Wrong you simpleton.");
+            $("#answer1").text("The correct answer was your face.");
+            $("#answer2").text("");
+            $("#answer3").text("");
+            $("#answer4").text("");
 
+        }
+    })
+    //randomly deciding answer 3 will be the first correct answer
+    $("#answer3").click(function () {
+        if (answersGuessed == 0) {
+            $("#question").text("Correct!");
+            answersRight++;
+            $("#answer1").text("");
+            console.log("test: " + count)
+        }
+    })
+    //if timer runs out
+
+    $(".answer").click(function () {
+        if (answersGuessed == 0) {
+            pause1();
+        }
+    })
+}
+
+function pause1() {
+    setTimeout(function () {
+        console.log("pause1 function ran")
+        slide2();
+
+
+    }, 4000)
+    answersGuessed++;
+
+}
+function slide2() {
+    console.log("hello");
+    $("#question").text(question2.q)
+    $("#answer1").text(question2.a1);
+    $("#answer2").text(question2.a2);
+    $("#answer3").text(question2.a3);
+    $("#answer4").text(question2.a4);
+
+
+
+    $(".answer").click(function () {
+        if (answersGuessed == 1) {
+            $("#question").text("Wrong you simpleton2.");
+            $("#answer1").text("The correct answer was your face2.");
+            $("#answer2").text("");
+            $("#answer3").text("");
+            $("#answer4").text("");
+        }
     })
 
+    $("#answer1").click(function () {
+        if (answersGuessed == 1) {
+            $("#question").text("Correct!");
+            answersRight++;
+            $("#answer1").text("");
+            console.log("test: " + count)
+        }
+    })
+    $(".answer").click(function () {
+        if (answersGuessed == 1) {
+            pause2();
+        }
+    })
+    count = 30;
+    intervalId = setInterval(timer, 1000);
+}
+
+function pause2() {
+    setTimeout(function () {
+        console.log("pause2 function ran")
+        slide3();
+
+
+    }, 4000)
+    answersGuessed++;
+
+}
+
+function slide3() {
+    $("#question").text(question3.q)
+    $("#answer1").text(question3.a1);
+    $("#answer2").text(question3.a2);
+    $("#answer3").text(question3.a3);
+    $("#answer4").text(question3.a4);
+    console.log("answersGuessed: " + answersGuessed)
+
+
+    $(".answer").click(function () {
+        if (answersGuessed == 2) {
+            $("#question").text("Wrong you simpleton2.");
+            $("#answer1").text("The correct answer was your face2.");
+            $("#answer2").text("");
+            $("#answer3").text("");
+            $("#answer4").text("");
+        }
+    })
+
+    $("#answer4").click(function () {
+        if (answersGuessed == 2) {
+            $("#question").text("Correct!");
+            answersRight++;
+            $("#answer1").text("");
+            console.log("test: " + count)
+        }
+    })
+    $(".answer").click(function () {
+        if (answersGuessed == 2) {
+            pause3();
+        }
+    })
+    count = 30;
+    intervalId = setInterval(timer, 1000);
+
+}
+
+function pause3() {
+    setTimeout(function () {
+        console.log("pause3 function ran")
+        slide4();
+
+
+    }, 4000)
+    answersGuessed++;
+
+}
+function slide4(){
+    
 }
